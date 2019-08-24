c8d673d2d7d66d81c4ea4499ea3d27da55e8501e
Puja <waglepuja@gmail.com>
2019-08-24 16:09:02 -0400

    some css styling

--- script.js
+++ script.js
@@ -1,99 +1,91 @@
 const entertainments ={
     movie:[
         {
-            title:'hello1',
-            genre1:'horror'
+            title:'Lucky you are still alive!',
+            genre1:'horror'            
         },
         {
-            title:'hello2',
+            title:'Do you really want to fight?',
             genre1:'action'
         },
         {
-            title:'hello3',
+            title:'Ahh let\'s not even go there',
             genre1:'romance'
         },
         {
-            title:'hello4',
+            title:'Super thrilling',
             genre1:'thriller'
         }
         
     ],
     games:[
         {
-            title:'hello5',
+            title:'Shoot\'em up',
             genre1:'shooter'
 
         },
         {
-            title:'hello6',
+            title:'Lucky you survived!',
             genre1:'survival'
 
         },
         {
-            title:'hello7',
+            title:'Catch me if you can',
             genre1:'platform'
 
         },
         {
-            title:'hello8',
+            title:'I\'m ready to fight, are you?',
             genre1:'fighting'
 
         }
     ]
 };
 
-
 $(document).ready(function(){
+    
 
     const getRandomItemFromArray = function(passedArray){
 		const randomNum = Math.floor(Math.random()* 
 		passedArray.length);
         return passedArray[randomNum];
-    }   
+    } 
 
     $('form').on('submit', function(event){
         event.preventDefault();
         // console.log('ready?');
- let userEntertainments =$('input[name="funTime"]:checked').val();
- let userGenre1 = $('input[name="genre"]:checked').val();
-
- console.log("HELLO",userEntertainments);
- console.log("hi",userGenre1);
-
-  
-//  if (userEntertainments !== ''){
-//      $('input:radio').val('');
-//  }
-   
- 
- const entertainmentsArray = entertainments[userEntertainments];
- console.log('array', entertainmentsArray);
-
- const filteredArray =[];
-
- for(let i=0; i < entertainmentsArray.length; i++ ){
-    //  console.log("opps",userGenre1);
-    //  console.log(entertainmentsArray[i].genre1);
-     if (userGenre1 === entertainmentsArray[i].genre1){
-         filteredArray.push(entertainmentsArray[i].title);
-     }
- }
-
-
- console.log("Filtered",filteredArray);
- 
-
- const finalResults = getRandomItemFromArray(filteredArray);
- console.log(finalResults);
-
- $('input:radio').prop("checked", false);
-
- $('.answer').html(`<h2 class ="choice">${finalResults}</h2>`)
-
-
         
+    let userEntertainments =$('input[name="funTime"]:checked').val();
+    let userGenre1 = $('input[name="genre"]:checked').val();
+
+    console.log("HELLO",userEntertainments);
+    console.log("hi",userGenre1);   
+
     
-});
+    entertainmentsArray = entertainments[userEntertainments];
+    console.log('array', entertainmentsArray);
+
+    const filteredArray =[];
+
+    for(let i=0; i < entertainmentsArray.length; i++ ){
+        //  console.log("opps",userGenre1);
+        //  console.log(entertainmentsArray[i].genre1);
+        if (userGenre1 === entertainmentsArray[i].genre1){
+            filteredArray.push(entertainmentsArray[i].title);
+        }
+    }
+
+    console.log("Filtered",filteredArray);
+    
+
+    const finalResults = getRandomItemFromArray(filteredArray);
+    console.log(finalResults);
+
+    $('input:radio').prop("checked", false);
+
+    $('.answer').html(`<h2 class ="choice">${finalResults}</h2>`)   
+
+});        
 
     
 });
