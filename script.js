ce44c065ee76d4288fe7a080f5d6ac87ce2557eb
Puja <waglepuja@gmail.com>
2019-08-26 00:34:56 -0400

    Checked

--- script.js
+++ script.js
@@ -64,65 +64,57 @@ $(document).ready(function(){
                 template = `<div class="genre1">
 
                 <p> Don't think about it, just pick one!</p>
+                <div class="labels1">
                 <div class="horror">
                     <input type="radio" name="genre" id="fun" value="horror" required>
                     <label for="fun">Horror</label>
-
                 </div>
                 <div class="action">
                     <input type="radio" name="genre" id="fun" value="action">
                     <label for="fun">Action</label>
-
                 </div>
                 <div class="romance">
                     <input type="radio" name="genre" id="fun" value="romance">
                     <label for="fun">Romance</label>
-
                 </div>
                 <div class="thriller">
                     <input type="radio" name="genre" id="fun" value="thriller">
                     <label for="fun">Thriller</label>
-
                 </div>
             </div>
             
             
             <div class="submitContainer">
             <input   type="submit" class="button" value="See my results!">
-        </div>
-            `;
+        </div>`;
             } else {
                 template = `<div class="genre2">
 
                 <p>Let's see what you want to play!</p>
-                <div class="shooter">
-                    <input type="radio" name="genre" id="fun" value="shooter">
-                    <label for="fun">Shooter</label>
-
+                    <div class="labels2">
+                    <div class="shooter">                        
+                        <input type="radio" name="genre" id="fun" value="shooter">
+                        <label for="fun">Shooter</label>
+                    </div>
+                    <div class="survival">
+                        <input type="radio" name="genre" id="fun" value="survival">
+                        <label for="fun">Survival</label>
+                    </div>
+                    <div class="platform">
+                        <input type="radio" name="genre" id="fun" value="platform">
+                        <label for="fun">Platform</label>
+                    </div>
+                    <div class="fighting">
+                        <input type="radio" name="genre" id="fun" value="fighting">
+                        <label for="fun">Fighting</label>
+                    </div>
                 </div>
-                <div class="survival">
-                    <input type="radio" name="genre" id="fun" value="survival">
-                    <label for="fun">Survival</label>
-
                 </div>
-                <div class="platform">
-                    <input type="radio" name="genre" id="fun" value="platform">
-                    <label for="fun">Platform</label>
-
-                </div>
-                <div class="fighting">
-                    <input type="radio" name="genre" id="fun" value="fighting">
-                    <label for="fun">Fighting</label>
-
-                </div>
-
 
             </div>
             <div class="submitContainer">
             <input   type="submit" class="button" value="See my results!">
-        </div>
-
-            `;
+        </div>`;
             }
             return template;
         };
@@ -133,47 +125,27 @@ $(document).ready(function(){
 
     $('form').on('submit', function(event){
         event.preventDefault();
-        // console.log('ready?');
-        
+               
     let userEntertainments =$('input[name="funTime"]:checked').val();
-    let userGenre1 = $('input[name="genre"]:checked').val();
-    
-    
-
-    // console.log("HELLO",userEntertainments);
-    // console.log("hi",userGenre1);   
-
-    
+    let userGenre1 = $('input[name="genre"]:checked').val();  
+         
     entertainmentsArray = entertainments[userEntertainments];
-    // console.log('array', entertainmentsArray);
-
+    
     const filteredArray =[];
 
-    for(let i=0; i < entertainmentsArray.length; i++ ){
-        //  console.log("opps",userGenre1);
-        //  console.log(entertainmentsArray[i].genre1);
+    for(let i=0; i < entertainmentsArray.length; i++ ){        
         if (userGenre1 === entertainmentsArray[i].genre1){
             filteredArray.push(entertainmentsArray[i].title);
         } 
         
-    }
-
-  
-    
-
-    
-    
-    // console.log("Filtered",filteredArray);
-    
-    
-    const finalResults = getRandomItemFromArray(filteredArray);
-    // console.log(finalResults);
+    }     
+        
+    const finalResults = getRandomItemFromArray(filteredArray);  
     
     $('input:radio').prop("checked", false);
     
-    $('.answer').html(`<h2 class ="choice">${finalResults}</h2>`)   
-    
-  
+    $('.answer').html(`<h2 class ="choice">${finalResults}</h2>`) 
+     
 });  
 
     
